d6d48393af747ecbbf2fae046d2f5a1630e8ebcf
tkharseh <tariq.kharseh@mail.utoronto.ca>
2022-09-04 22:46:34 -0400

    made updates

--- controllers/tables.js
+++ controllers/tables.js
@@ -1,7 +1,9 @@
 import Item from "../models/schemas.js";
 import { MongoClient, ObjectId } from "mongodb";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config({ path: ".env" });
 const databasename = "Inventory";
 const connection = MongoClient.connect(process.env.CONNECTION_URL);
 
--- index.js
+++ index.js
@@ -2,9 +2,10 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import tablesRoutes from "./routes/tables.js";
+import dotenv from "dotenv";
 
 const app = express();
-
+dotenv.config({path: '.env'})
 app.use(cors());
 app.use(express.json());
 app.use("/tables", tablesRoutes);
